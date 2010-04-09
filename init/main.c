824f15d961fea224531d3b17c70f7eb10d39c9f3
Bruce Ashfield <bruce.ashfield@windriver.com>
2010-04-08 23:44:21 -0700

    check console device file on fs when booting
    If a root filesystem is generated as non-root, one of
    the tell tale signs is /dev/console not being a character
    file. To save a whole class of questions, let's just test
    for the condition and let the user know.
    
    Signed-off-by: Richard Laroque <rlarocqu@windriver.com>
    Signed-off-by: Bruce Ashfield <bruce.ashfield@windriver.com>

--- init/main.c
+++ init/main.c
@@ -1459,6 +1459,18 @@ static int __ref kernel_init(void *unused)
 
 void console_on_rootfs(void)
 {
+#ifndef CONFIG_BLK_DEV_INITRD
+       /*
+        * Use /dev/console to infer if the rootfs is setup properly.
+        * In case of initrd or initramfs /dev/console might be instantiated
+        * later by /init so don't do this check for CONFIG_BLK_DEV_INITRD
+        */
+	if (vfs_lstat((char __user *) "/dev/console", (struct kstat __user *) &console_stat)
+			|| !S_ISCHR(console_stat.mode)) {
+		panic("/dev/console is missing or not a character device!\nPlease ensure your rootfs is properly configured\n");
+	}
+#endif
+
 	/* Open the /dev/console as stdin, this should never fail */
 	if (ksys_open((const char __user *) "/dev/console", O_RDWR, 0) < 0)
 		pr_err("Warning: unable to open an initial console.\n");
@@ -1470,6 +1482,9 @@ void console_on_rootfs(void)
 
 static noinline void __init kernel_init_freeable(void)
 {
+#ifndef CONFIG_BLK_DEV_INITRD
+	struct kstat console_stat;
+#endif
 	/*
 	 * Wait until kthreadd is all set-up.
 	 */
