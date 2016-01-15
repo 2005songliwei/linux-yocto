a047d5dc0cc877b56ef69f16fe4b9cc2f3d9f5a7
Michal Simek <michal.simek@xilinx.com>
2016-01-15 13:24:55 +0100

    net: phy: dp83867: Enable automatic crossover mode
    Enable automatic crossover cable detection.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    [Quanyang: original patch taken from
    https://github.com/Xilinx/linux-xlnx.git xlnx_rebase_v4.19]
    Signed-off-by: Quanyang Wang <quanyang.wang@windriver.com>

--- drivers/net/phy/dp83867.c
+++ drivers/net/phy/dp83867.c
@@ -79,6 +79,8 @@
 #define DP83867_PHYCR_FIFO_DEPTH_SHIFT		14
 #define DP83867_PHYCR_FIFO_DEPTH_MAX		0x03
 #define DP83867_PHYCR_FIFO_DEPTH_MASK		GENMASK(15, 14)
+#define DP83867_MDI_CROSSOVER		5
+#define DP83867_MDI_CROSSOVER_AUTO	0b10
 #define DP83867_PHYCR_RESERVED_MASK		BIT(11)
 
 /* RGMIIDCTL bits */
@@ -314,6 +316,12 @@ static int dp83867_config_init(struct phy_device *phydev)
 				   BIT(7));
 
 	if (phy_interface_is_rgmii(phydev)) {
+		ret = phy_write(phydev, MII_DP83867_PHYCTRL,
+			(DP83867_MDI_CROSSOVER_AUTO << DP83867_MDI_CROSSOVER) |
+			(dp83867->fifo_depth << DP83867_PHYCR_FIFO_DEPTH_SHIFT));
+		if (ret)
+			return ret;
+
 		val = phy_read(phydev, MII_DP83867_PHYCTRL);
 		if (val < 0)
 			return val;
