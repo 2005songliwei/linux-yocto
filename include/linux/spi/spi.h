301aa564d4e7065c611c641dfeae52a7b81b2c0b
P L Sai Krishna <lakshmi.sai.krishna.potthuri@xilinx.com>
2016-03-18 22:51:02 +0530

    mtd: Added dummy entry in the spi_transfer structure
    Added dummy entry in the spi_transfer structure.
    
    Signed-off-by: P L Sai Krishna <lakshmis@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    [Quanyang: original patch taken from
    https://github.com/Xilinx/linux-xlnx.git xlnx_rebase_v4.19]
    Signed-off-by: Quanyang Wang <quanyang.wang@windriver.com>

--- include/linux/spi/spi.h
+++ include/linux/spi/spi.h
@@ -792,6 +792,7 @@ extern void spi_res_release(struct spi_controller *ctlr,
  * @len: size of rx and tx buffers (in bytes)
  * @speed_hz: Select a speed other than the device default for this
  *      transfer. If 0 the default (from @spi_device) is used.
+ * @dummy: number of dummy cycles.
  * @bits_per_word: select a bits_per_word other than the device default
  *      for this transfer. If 0 the default (from @spi_device) is used.
  * @cs_change: affects chipselect after this transfer completes
@@ -922,6 +923,7 @@ struct spi_transfer {
 	struct spi_delay	cs_change_delay;
 	struct spi_delay	word_delay;
 	u32		speed_hz;
+	u32		dummy;
 
 	u32		effective_speed_hz;
 
