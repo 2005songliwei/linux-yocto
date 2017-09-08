c4d86c442227b25f40feaddec63cbd324ed2c74a
Kedareswara rao Appana <appana.durga.rao@xilinx.com>
2017-09-08 20:00:47 +0530

    dmaengine: xilinx_dma: Fix issues with vdma mulit fstore configuration
    This commit
    ie: 'commit 4f143cb03aba ("dmaeninge: xilinx_dma: Fix bug in multiple
    frame stores scenario in vdma")'
    fixes issues with multiple fstore by using circular mode feature.
    This implementation has a limitation as user needs to enable a hidden
    configuration option(c_debug_all) in the IP while creating the design.
    
    If user not aware of this h/w option and submits more frames
    then driver throughs a warning asking to enable the
    hidden configuration option.
    
    This patches fixes these issues by using the park mode feature.
    With this patch driver continuously parks through frame buffers
    based on the number of frames user submitted.
    
    Signed-off-by: Kedareswara rao Appana <appanad@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    [Quanyang: original patch taken from
    https://github.com/Xilinx/linux-xlnx.git xlnx_rebase_v4.19]
    Signed-off-by: Quanyang Wang <quanyang.wang@windriver.com>

--- drivers/dma/xilinx/xilinx_dma.c
+++ drivers/dma/xilinx/xilinx_dma.c
@@ -2793,6 +2793,7 @@ static int xilinx_dma_chan_probe(struct xilinx_dma_device *xdev,
 
 		chan->ctrl_offset = XILINX_DMA_MM2S_CTRL_OFFSET;
 		if (xdev->dma_config->dmatype == XDMA_TYPE_VDMA) {
+			chan->config.park = 1;
 			chan->desc_offset = XILINX_VDMA_MM2S_DESC_OFFSET;
 			chan->config.park = 1;
 
@@ -2823,6 +2824,7 @@ static int xilinx_dma_chan_probe(struct xilinx_dma_device *xdev,
 			chan->ctrl_offset = XILINX_DMA_S2MM_CTRL_OFFSET;
 
 		if (xdev->dma_config->dmatype == XDMA_TYPE_VDMA) {
+			chan->config.park = 1;
 			chan->desc_offset = XILINX_VDMA_S2MM_DESC_OFFSET;
 			chan->config.park = 1;
 
