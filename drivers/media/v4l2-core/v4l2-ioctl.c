d3ce1cd0104ecf2b4c80a4c10399971bba28ba1e
Vishal Sagar <vishal.sagar@xilinx.com>
2019-03-07 04:13:21 -0800

    media: v4l2-ioctl: Add 12 and 16 bpc RGB pixel formats
    This patch adds V4L2_PIX_FMT_XBGR40 and V4L2_PIX_FMT_BGR48 pixel
    formats.
    
    Signed-off-by: Vishal Sagar <vishal.sagar@xilinx.com>
    Reviewed-by: Hyun Kwon <hyun.kwon@xilinx.com>
    Reviewed-by: Satish Kumar Nagireddy <satish.nagireddy.nagireddy@xilinx.com>
    [Quanyang: original patch taken from
    https://github.com/Xilinx/linux-xlnx.git xlnx_rebase_v4.19]
    Signed-off-by: Quanyang Wang <quanyang.wang@windriver.com>

--- drivers/media/v4l2-core/v4l2-ioctl.c
+++ drivers/media/v4l2-core/v4l2-ioctl.c
@@ -1214,7 +1214,9 @@ static void v4l_fill_fmtdesc(struct v4l2_fmtdesc *fmt)
 	case V4L2_PIX_FMT_RGB32:	descr = "32-bit A/XRGB 8-8-8-8"; break;
 	case V4L2_PIX_FMT_ARGB32:	descr = "32-bit ARGB 8-8-8-8"; break;
 	case V4L2_PIX_FMT_XRGB32:	descr = "32-bit XRGB 8-8-8-8"; break;
-	case V4L2_PIX_FMT_XBGR30: 	descr = "32-bit XBGR 2-10-10-10"; break;
+	case V4L2_PIX_FMT_XBGR30:	descr = "32-bit XBGR 2-10-10-10"; break;
+	case V4L2_PIX_FMT_XBGR40:	descr = "40-bit XBGR 4-12-12-12"; break;
+	case V4L2_PIX_FMT_BGR48:	descr = "48-bit BGR 16-16-16"; break;
 	case V4L2_PIX_FMT_BGRA32:	descr = "32-bit ABGR 8-8-8-8"; break;
 	case V4L2_PIX_FMT_BGRX32:	descr = "32-bit XBGR 8-8-8-8"; break;
 	case V4L2_PIX_FMT_RGBA32:	descr = "32-bit RGBA 8-8-8-8"; break;
