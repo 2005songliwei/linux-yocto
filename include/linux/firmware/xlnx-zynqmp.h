0af08140db107479b9ecdeb80055619c56efa00f
Michal Simek <michal.simek@xilinx.com>
2019-03-27 10:22:52 +0100

    firmware: zynqmp: Add pm_config_reg_access firmware call
    Provide access for configuration CSU/PMU registers.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    [Quanyang: original patch taken from
    https://github.com/Xilinx/linux-xlnx.git xlnx_rebase_v4.19]
    Signed-off-by: Quanyang Wang <quanyang.wang@windriver.com>

--- include/linux/firmware/xlnx-zynqmp.h
+++ include/linux/firmware/xlnx-zynqmp.h
@@ -98,7 +98,9 @@ enum pm_api_id {
 	PM_CLOCK_SETPARENT,
 	PM_CLOCK_GETPARENT,
 	PM_SECURE_AES = 47,
-	PM_EFUSE_ACCESS = 53,
+	/* PM_REGISTER_ACCESS API */
+	PM_REGISTER_ACCESS = 52,
+	PM_EFUSE_ACCESS,
 	PM_FEATURE_CHECK = 63,
 	PM_API_MAX,
 };
@@ -476,6 +478,8 @@ struct zynqmp_eemi_ops {
 	int (*pinctrl_set_function)(const u32 pin, const u32 id);
 	int (*pinctrl_get_config)(const u32 pin, const u32 param, u32 *value);
 	int (*pinctrl_set_config)(const u32 pin, const u32 param, u32 value);
+	int (*register_access)(u32 register_access_id, u32 address,
+			       u32 mask, u32 value, u32 *out);
 	int (*aes)(const u64 address, u32 *out);
 	int (*efuse_access)(const u64 address, u32 *out);
 };
