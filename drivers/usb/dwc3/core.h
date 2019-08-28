8e7e70bb964f99d50074f3a769396fc785410b4a
Anurag Kumar Vulisha <anurag.kumar.vulisha@xilinx.com>
2019-08-28 13:27:56 +0000

    usb: dwc3: Add support for masking phy reset signal
    Versal usb controller drives phy reset signal during
    Soft Reset or Core Reset to issue reset to the ULPI
    phy. The ULPI phy needs to be resetted only at the time
    of intialization. This patch updates the code to provide
    a function for masking the phy reset signal from reaching
    the ULPI phy. This function is called only after the phy
    initialization is done.
    
    Signed-off-by: Anurag Kumar Vulisha <anurag.kumar.vulisha@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    [Quanyang: original patch taken from
    https://github.com/Xilinx/linux-xlnx.git xlnx_rebase_v4.19]
    Signed-off-by: Quanyang Wang <quanyang.wang@windriver.com>

--- drivers/usb/dwc3/core.h
+++ drivers/usb/dwc3/core.h
@@ -1266,6 +1266,7 @@ struct dwc3 {
 	unsigned		is_hibernated:1;
 
 	unsigned		dis_metastability_quirk:1;
+	unsigned		mask_phy_rst:1;
 
 	u16			imod_interval;
 	bool			is_d3;
@@ -1454,6 +1455,7 @@ void dwc3_simple_wakeup_capable(struct device *dev, bool wakeup);
 void dwc3_set_simple_data(struct dwc3 *dwc);
 void dwc3_simple_check_quirks(struct dwc3 *dwc);
 int dwc3_set_usb_core_power(struct dwc3 *dwc, bool on);
+void dwc3_mask_phy_reset(struct device *dev, bool mask);
 #else
 static inline int dwc3_enable_hw_coherency(struct device *dev)
 { return 1; }
@@ -1464,6 +1466,8 @@ void dwc3_set_simple_data(struct dwc3 *dwc)
 void dwc3_simple_check_quirks(struct dwc3 *dwc)
 { ; }
 int dwc3_set_usb_core_power(struct dwc3 *dwc, bool on)
+{ ; }
+void dwc3_mask_phy_reset(struct device *dev, bool mask)
 { ; }
 #endif
 
