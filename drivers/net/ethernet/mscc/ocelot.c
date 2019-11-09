6591ea240263e227551fb823ec4a5c11925a4b00
Claudiu Manoil <claudiu.manoil@nxp.com>
2019-11-09 15:02:58 +0200

    net: mscc: ocelot: initialize list of multicast addresses in common code
    commit  from
    https://source.codeaurora.org/external/imx/linux-imx
    
    This is just common path code that belongs to ocelot_init,
    it has nothing to do with a specific SoC/board instance.
    
    Signed-off-by: Claudiu Manoil <claudiu.manoil@nxp.com>
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Xiaolei Wang <xiaolei.wang@windriver.com>

--- drivers/net/ethernet/mscc/ocelot.c
+++ drivers/net/ethernet/mscc/ocelot.c
@@ -2247,6 +2247,7 @@ int ocelot_init(struct ocelot *ocelot)
 	if (!ocelot->stats_queue)
 		return -ENOMEM;
 
+	INIT_LIST_HEAD(&ocelot->multicast);
 	ocelot_mact_init(ocelot);
 	ocelot_vlan_init(ocelot);
 	ocelot_ace_init(ocelot);
