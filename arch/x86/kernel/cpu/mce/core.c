74da9a556d9a7af8d0a64ba65c78a4a86532c722
Jan H. Schönherr <jschoenh@amazon.de>
2019-12-10 01:07:32 +0100

    x86/mce: Pass MCE message to mce_panic() on failed kernel recovery
    commit 2d806d0723589a726b48b88e0f53988849dc03d3 upstream.
    
    In commit
    
      b2f9d678e28c ("x86/mce: Check for faults tagged in EXTABLE_CLASS_FAULT exception table entries")
    
    another call to mce_panic() was introduced. Pass the message of the
    handled MCE to that instance of mce_panic() as well, as there doesn't
    seem to be a reason not to.
    
    Signed-off-by: Jan H. Schönherr <jschoenh@amazon.de>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Reviewed-by: Tony Luck <tony.luck@intel.com>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: linux-edac <linux-edac@vger.kernel.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: x86-ml <x86@kernel.org>
    Link: https://lkml.kernel.org/r/20191210000733.17979-6-jschoenh@amazon.de
    Signed-off-by: Liwei Song <liwei.song@windriver.com>

--- arch/x86/kernel/cpu/mce/core.c
+++ arch/x86/kernel/cpu/mce/core.c
@@ -1410,7 +1410,7 @@ void do_machine_check(struct pt_regs *regs, long error_code)
 		ist_end_non_atomic();
 	} else {
 		if (!fixup_exception(regs, X86_TRAP_MC, error_code, 0))
-			mce_panic("Failed kernel mode recovery", &m, NULL);
+			mce_panic("Failed kernel mode recovery", &m, msg);
 	}
 
 out_ist:
