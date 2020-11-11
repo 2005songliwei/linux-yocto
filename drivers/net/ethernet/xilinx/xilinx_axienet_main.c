59b0ea17cf6461ca6bc39c582417de48ec99df63
He Zhe <zhe.he@windriver.com>
2020-11-11 11:31:23 +0800

    net: axienet: Add missing return value type for __axienet_device_reset
    To fix the following build warnings and the related work flow.
    
    drivers/net/ethernet/xilinx/xilinx_axienet_main.c:409:11: warning:
    'return' with a value, in function returning void [-Wreturn-type]
    
    drivers/net/ethernet/xilinx/xilinx_axienet_main.c:413:9: warning:
    'return' with a value, in function returning void [-Wreturn-type]
    
    Fixes: 00c44b7e5c15 ("Merge branch 'v5.4/standard/base' into v5.4/standard/xlnx-soc")
    Signed-off-by: He Zhe <zhe.he@windriver.com>
    Signed-off-by: Bruce Ashfield <bruce.ashfield@gmail.com>

--- drivers/net/ethernet/xilinx/xilinx_axienet_main.c
+++ drivers/net/ethernet/xilinx/xilinx_axienet_main.c
@@ -387,7 +387,7 @@ static void xxvenet_setoptions(struct net_device *ndev, u32 options)
 }
 
 
-void __axienet_device_reset(struct axienet_dma_q *q)
+static int __axienet_device_reset(struct axienet_dma_q *q)
 {
 	u32 timeout;
 
